6ec138e9ef217bc46268818a57598588ad39f329
judevercetti <judevercetti@gmail.com>
2023-06-11 14:54:49 +0300

    build

--- public/build/assets/jsx-runtime-a170c46f.js
+++ public/build/assets/jsx-runtime-a170c46f.js
@@ -1,4 +1,4 @@
-import{r as i}from"./app-cc2e2461.js";var o={},u={get exports(){return o},set exports(t){o=t}},n={};/**
+import{r as i}from"./app-b25f4611.js";var o={},u={get exports(){return o},set exports(t){o=t}},n={};/**
  * @license React
  * react-jsx-runtime.production.min.js
  *
